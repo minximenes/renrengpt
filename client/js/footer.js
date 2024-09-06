0774020b7b2bf77e028719c28ad6919d7d67cbf5
unknown <min.ximenes@gmail.com>
2024-09-06 11:20:41 +0800

    baidu tongji

--- client/js/footer.js
+++ client/js/footer.js
@@ -7,3 +7,11 @@
         });
     };
 })();
+/* baidu tongji */
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?d27c9f0b668831b7cc71379502d7af8a";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
